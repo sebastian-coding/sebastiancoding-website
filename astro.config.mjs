420729aa0c01f2ae87418e5725a699657b606488
Sebastian Cardozo <sebastian@sebastiancoding.com>
2024-12-26 20:24:39 -0500

    Add GTAG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sebastiancoding.com',
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), partytown({
+        config: {
+            forward: ['dataLayer.push']
+        }
+    })],
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'es'],
